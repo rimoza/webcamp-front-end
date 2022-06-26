320eae2729a5ce16606ee8e75bbb3f9af66c055a
RidwanMohmet <ridwan.maxamed1@gmail.com>
2022-06-26 13:37:00 +0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import WebcamCapture from "./components/WebcamCapture/WebcamCapture";
 export default function App() {
   return (
     <div className="bg-blue-100 max-w-screen h-screen max-h-screen flex flex-col  items-center  ">
-      {/* <Header /> */}
+      <Header />
       <div className="mt-10 flex justify-center items-center ">
         <Card>
           <WebcamCapture />
         </Card>
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 const Footer = () => {
+  const date = new Date();
   return (
     <div className="absolute bottom-0 left-0 right-0 bg-blue-500 py-3">
-      <h3 className="text-center text-3xl text-white font-bold">Footer</h3>
+      <h5 className="text-center text-2xl text-white font-bold">
+        &copy; <a href="bigiltech.com">Bigiltech @ {date.getFullYear()}</a>
+      </h5>
     </div>
   );
 };
