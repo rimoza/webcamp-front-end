7aaaf6ca92e94ec6550c099d38886abd080e77f0
RidwanMohmet <ridwan.maxamed1@gmail.com>
2022-07-05 10:06:20 +0300

    Minor fix

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -3,10 +3,15 @@ import React from "react";
 const Footer = () => {
   const date = new Date();
   return (
-    <div className="absolute bottom-0 left-0 right-0 bg-blue-500 py-3">
-      <h5 className="text-center text-2xl text-white font-bold">
+    <div className="absolute bottom-0 left-0 right-0 text-center bg-blue-500 py-3">
+      <a
+        href="http://bigiltech.com"
+        target="_blank"
+        rel="noreferrer"
+        className="text-center text-2xl text-white font-bold"
+      >
         &copy; <a href="bigiltech.com">Bigiltech @ {date.getFullYear()}</a>
-      </h5>
+      </a>
     </div>
   );
 };
